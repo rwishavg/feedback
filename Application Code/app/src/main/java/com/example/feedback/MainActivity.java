99825185e0629e36035fbfce907fea8d72ba554d
Aniket Gautam <aniket.gautam300@gmail.com>
2020-02-09 12:27:28 +0530

    New page

--- Application Code/app/src/main/java/com/example/feedback/MainActivity.java
+++ Application Code/app/src/main/java/com/example/feedback/MainActivity.java
@@ -2,25 +2,35 @@ package com.example.feedback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
+    private Button button;
     private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.button_teacher);
+
+        //  button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  openP2();
+            }
+        });
     }
 
-    button1 = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            openActivity2();
-        }
-    });
+    public void openP2() {
+        Intent intent = new Intent(this, P2.class);
+        startActivity(intent);
+    }
 }
